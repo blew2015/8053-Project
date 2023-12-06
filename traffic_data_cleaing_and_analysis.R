f27e6298dba2dd9165c3ec3b11f406e50dc002d7
blew2015 <ben.lewis9004@gmail.com>
2023-12-06 14:15:04 -0600

    bootstrap changes

--- traffic_data_cleaing_and_analysis.R
+++ traffic_data_cleaing_and_analysis.R
@@ -12,7 +12,7 @@ count_by_month_2 <- accidents %>% mutate(
   filter(Year == 2014, Speed_limit < 60, Speed_limit > 45) %>% 
   group_by(Year, Month, X1st_Road_Number, X1st_Road_Class,  X2nd_Road_Number, X2nd_Road_Class) %>%
   summarise(count = n()) %>% 
-  filter(Year == "2014", Month == "01") 
+  filter(Year == "2014") 
 
 write.csv(count_by_month_2, "~/Documents/GitHub/8053-Project/Accidents_filtered_01_2014_2.csv")
 
@@ -34,7 +34,7 @@ tru_alpha <- 1
 #   eb_se_vec[i-9] <- (perm[i+1,]$count - emp_b_est_lam)^2
 #   f_se_vec[i-9] <- (perm[i+1,]$count - f_b_est_lam)^2
 # }
-B = 1000
+B = 5000
 mat_f <- matrix(data = NA, nrow = nrow(count_by_month_2) - 10, ncol = B)
 mat_e <- matrix(data = NA, nrow = nrow(count_by_month_2) - 10, ncol = B)
 ### B = 100 
@@ -43,12 +43,14 @@ for (b in 1:B){
     ### Get bootstrapped sample 
     bstrap_ind <- sample(1:nrow(count_by_month_2), size = i, replace = TRUE)
     bstrap_set <- count_by_month_2[bstrap_ind, ]
+    # bstrap_set$count
     test_set <- count_by_month_2[-bstrap_ind,]
     
     ### Calculate the empirical bayes estimates
-    emp_b_est_b <- var(bstrap_set$count)/mean(bstrap_set$count)
-    emp_b_est_a <- mean(bstrap_set$count)^2/var(bstrap_set$count)
+    emp_b_est_b <- (var(bstrap_set$count) + 1/i)/mean(bstrap_set$count)
+    emp_b_est_a <- mean(bstrap_set$count)^2/(var(bstrap_set$count) + 1/i)
     emp_b_est_lam <- emp_b_est_a*emp_b_est_b
+    emp_b_est_lam
     
     ### Calculate the hierarchical bayes estimates
     f_b_est_beta <- ((1/tru_beta) + nrow(bstrap_set))^(-1)
@@ -63,6 +65,7 @@ for (b in 1:B){
   }
 }
 
+par(mfrow = c(2, 1))
 plot(rowMeans(mat_e, na.rm = TRUE))
 plot(rowMeans(mat_f, na.rm = TRUE))
 
